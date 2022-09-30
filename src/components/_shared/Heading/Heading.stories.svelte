f8bedb64127cdabf6d0ea45c2eeb2e684b248498
Tom Nightingale <tomnightingale@thisissoon.com>
2022-09-30 11:46:39 +0100

    fix(Commit): Linting

---
+++ src/components/_shared/Heading/Heading.stories.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import Heading from "./Heading.svelte";
+</script>
+
+<Meta
+  title="Components/Heading"
+  component={Heading}
+  argTypes={{
+    component: { control: "text" },
+    classes: {control: "text"},
+  }}
+/>
+
+<Template let:args>
+  <Heading {...args} />
+</Template>
+
+<Story name="h1">
+  <Heading component="h1">Heading one.</Heading>
+</Story>
+
+<Story name="h2">
+  <Heading component="h2">Heading two.</Heading>
+</Story>
+
+<Story name="h3">
+  <Heading component="h3">Heading three.</Heading>
+</Story>
+
+<Story name="h4">
+  <Heading component="h4">Heading four.</Heading>
+</Story>
+
+<Story name="h5">
+  <Heading component="h5">Heading five.</Heading>
+</Story>
+
+<Story name="h6">
+  <Heading component="h6">Heading six.</Heading>
+</Story>
+
+
---
+++ src/components/_shared/Heading/Heading.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  export let component: string = "h1";
+  export let classes: string = "";
+</script>
+
+{#if component === "h1"}
+  <h1 class="h1">
+    <slot />
+  </h1>
+{:else if component === "h2"}
+  <h2 class={classes} {...$$props}>
+    <slot />
+  </h2>
+{:else if component === "h3"}
+  <h3 class={classes} {...$$props}>
+    <slot />
+  </h3>
+{:else if component === "h4"}
+  <h4 class={classes} {...$$props}>
+    <slot />
+  </h4>
+{:else if component === "h5"}
+  <h5 class={classes} {...$$props}>
+    <slot />
+  </h5>
+{:else if component === "h6"}
+  <h6 class={classes} {...$$props}>
+    <slot />
+  </h6>
+{/if}
+
+<style>
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    @apply font-bold;
+  }
+
+  h1,
+  .h1 {
+    @apply text-5xl;
+  }
+  h2 {
+    @apply text-4xl;
+  }
+  h3 {
+    @apply text-3xl;
+  }
+  h4 {
+    @apply text-2xl;
+  }
+  h5 {
+    @apply text-xl;
+  }
+  h6 {
+    @apply text-lg;
+  }
+</style>
