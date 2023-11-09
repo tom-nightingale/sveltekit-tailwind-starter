21cdbcf3c4d42b8d423d502fefef6c85c39e1c43
Tom Nightingale <tom@weareleague.co.uk>
2023-11-09 16:33:37 +0000

    feat(Typography): Use class directive for props

--- src/lib/components/Typography/Typography.svelte
+++ src/lib/components/Typography/Typography.svelte
@@ -6,82 +6,93 @@
   export let underline = false;
   export let bold = false;
   export let italic = false;
+  export let lineThrough = false;
 </script>
 
 {#if component === "h1"}
   <h1
-    class={`${variant} ${uppercase && "uppercase"} ${
-      underline && "underline"
-    } ${bold && "font-bold"} ${italic && "italic"} ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
   </h1>
 {:else if component === "h2"}
   <h2
-    class={`${variant} ${uppercase && "uppercase"} ${
-      underline && "underline"
-    } ${bold && "font-bold"} ${italic && "italic"} ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
   </h2>
 {:else if component === "h3"}
   <h3
-    class={`${variant} ${uppercase && "uppercase"}
-    ${underline && "underline"} ${bold && "font-bold"} ${
-      italic && "italic"
-    } ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
   </h3>
 {:else if component === "h4"}
   <h4
-    class={`${variant} ${uppercase && "uppercase"}
-    ${underline && "underline"} ${bold && "font-bold"} ${
-      italic && "italic"
-    } ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
+    class:theme
     {...$$props}
   >
     <slot />
   </h4>
 {:else if component === "h5"}
   <h5
-    class={`${variant} ${uppercase && "uppercase"}
-    ${underline && "underline"} ${bold && "font-bold"} ${
-      italic && "italic"
-    } ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
   </h5>
 {:else if component === "h6"}
   <h6
-    class={`${variant} ${uppercase && "uppercase"}
-    ${underline && "underline"} ${bold && "font-bold"} ${
-      italic && "italic"
-    } ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
   </h6>
 {:else if component === "p"}
   <p
-    class={`${variant} ${uppercase && "uppercase"}
-    ${underline && "underline"} ${bold && "font-bold"} ${
-      italic && "italic"
-    } ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
   </p>
 {:else if component === "span"}
   <span
-    class={`${variant} ${uppercase && "uppercase"}
-    ${underline && "underline"} ${bold && "font-bold"} ${
-      italic && "italic"
-    } ${theme}`}
+    class={`${variant} ${theme} ${lineThrough && "line-through"}`}
+    class:uppercase
+    class:underline
+    class:italic
+    class:bold
     {...$$props}
   >
     <slot />
